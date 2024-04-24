ee15d276bd4f21ec1ca41018d15f88f3f99f2990
andres-ordonezs <andresmw.89@gmail.com>
2024-04-24 14:44:48 -0400

    num-greater

--- trees/num-greater/numGreater.ts
+++ trees/num-greater/numGreater.ts
@@ -1,11 +1,17 @@
-import { TreeNodeNum } from "../common/tree";
+import {TreeNodeNum} from "../common/tree";
 
 /** numGreater(lowerBound): starting from the invoking node and moving
  * through its children, return a count of the number of nodes whose value
  * is greater than lowerBound. */
 
 function numGreater(node: TreeNodeNum, lowerBound: number): number {
-  return 42;
+  let count = node.val > lowerBound ? 1 : 0;
+
+  for (const child of node.children) {
+    count += numGreater(child, lowerBound);
+  }
+
+  return count;
 }
 
-export { numGreater };
+export {numGreater};
